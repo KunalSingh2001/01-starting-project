f1a16b186680d091b15c85ad4bd478472efad841
kunal <kunalsingh787711@gmail.com>
2025-08-21 22:15:22 +0530

    Applying the popup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, {Fragment} from "react";
 import Header from "./components/Layout/Header";
+import PopupCart from "./components/Outer/PopupCart";
 
 function App() {
   return (
     <Fragment>
         <Header/>
-          
+        <PopupCart/>
     </Fragment>
   );
 }
---
+++ src/components/Outer/PopupCart.js
@@ -0,0 +1,32 @@
+import React from "react";
+import classes from "./PopupCart.module.css";
+import ReactDOM from "react-dom";
+
+function PopupCart() {
+  const backdrop = (
+    <div className={classes["backdrop"]}></div>
+  );
+
+  const modal = (
+    <div className={classes["modal"]}>
+      <header>Sushi</header>
+      <div className={classes["content"]}>
+        <span>Total Amount</span>
+        <span>35.62</span>
+      </div>
+      <div className={classes["actions"]}>
+        <button className={classes["close"]}>Close</button>
+        <button className={classes["order"]}>Order</button>
+      </div>
+    </div>
+  );
+
+  return (
+    <>
+      {ReactDOM.createPortal(backdrop, document.getElementById("overlays"))}
+      {ReactDOM.createPortal(modal, document.getElementById("overlays"))}
+    </>
+  );
+}
+
+export default PopupCart;
